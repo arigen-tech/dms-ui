4115445f7b28c5c8ca6990e791724853924712ff
“HariomBhardwaj19” <hbstudent1997@gmail.com>
2025-06-06 18:05:36 +0530

    category css fix

--- src/Data/Category.js
+++ src/Data/Category.js
@@ -173,7 +173,7 @@ const Category = () => {
   };
 
   const confirmToggleActiveStatus = async () => {
-     setIsConfirmDisabled(true);
+    setIsConfirmDisabled(true);
 
     if (categoryToToggle) {
       try {
@@ -203,7 +203,7 @@ const Category = () => {
           setCategories(updatedCategories);
           setModalVisible(false);
           setCategoryToToggle(null);
-           setIsConfirmDisabled(false);
+          setIsConfirmDisabled(false);
 
           // Set success message
           showPopup('Category status changed successfully!', "success");
@@ -294,9 +294,9 @@ const Category = () => {
 
 
   return (
-    <div className="p-4">
-      <h1 className="text-xl mb-4 font-semibold">CATEGORIES</h1>
-      <div className="bg-white p-4 rounded-lg shadow-sm">
+    <div className="px-2">
+      <h1 className="text-lg mb-1 font-semibold">Categories</h1>
+      <div className="bg-white p-1 shadow-sm">
         {/* Popup Messages */}
         {popupMessage && (
           <Popup
@@ -305,204 +305,203 @@ const Category = () => {
             onClose={popupMessage.onClose}
           />
         )}
-        <div className="mb-4 bg-slate-100 p-4 rounded-lg">
-          <div className="grid grid-cols-3 gap-4">
-            <div className="flex flex-col">
-              <label htmlFor="name" className="mb-1 text-md font-medium text-gray-700">
+        <div className="mb-4 bg-slate-100 p-2 rounded-lg">
+          <div className="flex gap-6">
+            <div className="w-4/5 grid grid-cols-1 sm:grid-cols-2 gap-6">
+              <label htmlFor="name" className="block text-md font-medium text-gray-700">
                 Name
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Enter Name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  className="mt-1 block w-full p-2 border rounded-md outline-none focus:ring-2 focus:ring-blue-500"
+                />
               </label>
+            </div>
+            <div className="w-1/5 flex items-end">
+              {editingCategoryId === null ? (
+                // Button to add a new category
+                <button
+                  onClick={handleAddCategory}
+                  className="bg-blue-900 text-white rounded-2xl p-2 w-full text-sm flex items-center justify-center"
+                >
+                  <PlusCircleIcon className="h-5 w-5 mr-1" /> Add Category
+                </button>
+              ) : (
+                // Button to update an existing category
+                <button
+                  onClick={handleSaveEdit}
+                  className="bg-blue-900 text-white rounded-2xl p-2 w-full text-sm flex items-center justify-center"
+                >
+                  <CheckCircleIcon className="h-5 w-5 mr-1" /> Update
+                </button>
+              )}
+            </div>
+
+          </div>
+          </div>
+
+          <div className="mb-3 bg-slate-100 px-3 py-2 rounded-lg flex flex-col md:flex-row justify-between items-center gap-4">
+            {/* Items Per Page (50%) */}
+            <div className="flex items-center bg-blue-500 rounded-lg w-full flex-1 md:w-1/2">
+              <label
+                htmlFor="itemsPerPage"
+                className="mr-2 ml-2 text-white text-sm"
+              >
+                Show:
+              </label>
+              <select
+                id="itemsPerPage"
+                className="border rounded-r-lg p-1.5 outline-none w-full"
+                value={itemsPerPage}
+                onChange={(e) => {
+                  setItemsPerPage(Number(e.target.value));
+                  setCurrentPage(1);
+                }}
+              >
+                {[5, 10, 15, 20].map((num) => (
+                  <option key={num} value={num}>
+                    {num}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            {/* Search Input (Remaining Space) */}
+            <div className="flex items-center w-full md:w-auto flex-1">
               <input
                 type="text"
-                id="name"
-                name="name"
-                placeholder="Enter Name"
-                value={formData.name}
-                onChange={handleInputChange}
-                className="p-2 border rounded-md outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Search..."
+                className="border rounded-l-md p-1 outline-none w-full"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
               />
+              <MagnifyingGlassIcon className="text-white bg-blue-500 rounded-r-lg h-8 w-8 border p-1.5" />
             </div>
           </div>
 
-          <div className="mt-3 flex justify-start">
-            {editingCategoryId === null ? (
-              // Button to add a new category
-              <button
-                onClick={handleAddCategory}
-                className="bg-blue-900 text-white rounded-2xl p-2 flex items-center text-sm justify-center"
-              >
-                <PlusCircleIcon className="h-5 w-5 mr-1" /> Add Category
-              </button>
-            ) : (
-              // Button to update an existing category
-              <button
-                onClick={handleSaveEdit}
-                className="bg-blue-900 text-white rounded-2xl p-2 flex items-center text-sm justify-center"
-              >
-                <CheckCircleIcon className="h-5 w-5 mr-1" /> Update
-              </button>
-            )}
-          </div>
-
-        </div>
-
-        <div className="mb-4 bg-slate-100 p-4 rounded-lg flex flex-col md:flex-row justify-between items-center gap-4">
-          {/* Items Per Page (50%) */}
-          <div className="flex items-center bg-blue-500 rounded-lg w-full flex-1 md:w-1/2">
-            <label
-              htmlFor="itemsPerPage"
-              className="mr-2 ml-2 text-white text-sm"
-            >
-              Show:
-            </label>
-            <select
-              id="itemsPerPage"
-              className="border rounded-r-lg p-1.5 outline-none w-full"
-              value={itemsPerPage}
-              onChange={(e) => {
-                setItemsPerPage(Number(e.target.value));
-                setCurrentPage(1);
-              }}
-            >
-              {[5, 10, 15, 20].map((num) => (
-                <option key={num} value={num}>
-                  {num}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          {/* Search Input (Remaining Space) */}
-          <div className="flex items-center w-full md:w-auto flex-1">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="border rounded-l-md p-1 outline-none w-full"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <MagnifyingGlassIcon className="text-white bg-blue-500 rounded-r-lg h-8 w-8 border p-1.5" />
-          </div>
-        </div>
-
-        <div className="overflow-x-auto">
-          <table className="w-full border-collapse border">
-            <thead>
-              <tr className="bg-slate-100">
-                <th className="border p-2 text-left">Sr</th>
-                <th className="border p-2 text-left">Category</th>
-                <th className="border p-2 text-left">Created On</th>
-                <th className="border p-2 text-left">Updated On</th>
-                <th className="border p-2 text-left">Status</th>
-                <th className="border p-2 text-left">Edit</th>
-                <th className="border p-2 text-left">Access</th>
-              </tr>
-            </thead>
-            <tbody>
-              {paginatedCategories.map((category, index) => (
-                <tr key={category.id}>
-                  <td className="border p-2">{(currentPage - 1) * itemsPerPage + index + 1}</td>
-                  <td className="border p-2">{category.name}</td>
-                  <td className="border p-2">{formatDate(category.createdOn)}</td>
-                  <td className="border p-2">{formatDate(category.updatedOn)}</td>
-                  <td className="border p-2">{category.active === true ? 'Active' : 'Inactive'}</td>
-                  <td className="border p-2">
-                    <button onClick={() => handleEditCategory(category.id)} disabled={category.active === false}
-                      className={`${category.active === false ? 'opacity-50 cursor-not-allowed' : ''}`}>
-                      <PencilIcon className="h-6 w-6 text-white bg-yellow-400 rounded-xl p-1" />
-                    </button>
-                  </td>
-                  <td className="border p-2">
-                    <button
-                      onClick={() => handleToggleActiveStatus(category)}
-                      className={`p-1 rounded-full ${category.active === true ? 'bg-green-500' : 'bg-red-500'}`}
-                    >
-                      {category.active === true ? (
-                        <LockOpenIcon className="h-5 w-5 text-white p-0.5" />
-                      ) : (
-                        <LockClosedIcon className="h-5 w-5 text-white p-0.5" />
-                      )}
-                    </button>
-                  </td>
+          <div className="overflow-x-auto">
+            <table className="w-full border-collapse border">
+              <thead>
+                <tr className="bg-slate-100">
+                  <th className="border p-2 text-left">Sr</th>
+                  <th className="border p-2 text-left">Category</th>
+                  <th className="border p-2 text-left">Created On</th>
+                  <th className="border p-2 text-left">Updated On</th>
+                  <th className="border p-2 text-left">Status</th>
+                  <th className="border p-2 text-left">Edit</th>
+                  <th className="border p-2 text-left">Access</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {paginatedCategories.map((category, index) => (
+                  <tr key={category.id}>
+                    <td className="border p-2">{(currentPage - 1) * itemsPerPage + index + 1}</td>
+                    <td className="border p-2">{category.name}</td>
+                    <td className="border p-2">{formatDate(category.createdOn)}</td>
+                    <td className="border p-2">{formatDate(category.updatedOn)}</td>
+                    <td className="border p-2">{category.active === true ? 'Active' : 'Inactive'}</td>
+                    <td className="border p-2">
+                      <button onClick={() => handleEditCategory(category.id)} disabled={category.active === false}
+                        className={`${category.active === false ? 'opacity-50 cursor-not-allowed' : ''}`}>
+                        <PencilIcon className="h-6 w-6 text-white bg-yellow-400 rounded-xl p-1" />
+                      </button>
+                    </td>
+                    <td className="border p-2">
+                      <button
+                        onClick={() => handleToggleActiveStatus(category)}
+                        className={`p-1 rounded-full ${category.active === true ? 'bg-green-500' : 'bg-red-500'}`}
+                      >
+                        {category.active === true ? (
+                          <LockOpenIcon className="h-5 w-5 text-white p-0.5" />
+                        ) : (
+                          <LockClosedIcon className="h-5 w-5 text-white p-0.5" />
+                        )}
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
 
-        <div className="flex items-center mt-4">
-          {/* Previous Button */}
-          <button
-            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-            disabled={currentPage === 1}
-            className={`px-3 py-1 rounded mr-3 ${currentPage === 1 ? "bg-gray-300 cursor-not-allowed" : "bg-slate-200 hover:bg-slate-300"
-              }`}
-          >
-            <ArrowLeftIcon className="inline h-4 w-4 mr-2 mb-1" />
-            Previous
-          </button>
-
-          {/* Page Number Buttons */}
-          {getPageNumbers().map((page) => (
+          <div className="flex items-center mt-4">
+            {/* Previous Button */}
             <button
-              key={page}
-              onClick={() => setCurrentPage(page)}
-              className={`px-3 py-1 rounded mx-1 ${currentPage === page ? "bg-blue-500 text-white" : "bg-slate-200 hover:bg-blue-100"
+              onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+              disabled={currentPage === 1}
+              className={`px-3 py-1 rounded mr-3 ${currentPage === 1 ? "bg-gray-300 cursor-not-allowed" : "bg-slate-200 hover:bg-slate-300"
                 }`}
             >
-              {page}
+              <ArrowLeftIcon className="inline h-4 w-4 mr-2 mb-1" />
+              Previous
             </button>
-          ))}
 
-          {/* Page Count Info */}
-          <span className="text-sm text-gray-700 mx-2">of {totalPages} pages</span>
-
-          {/* Next Button */}
-          <button
-            onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
-            disabled={currentPage === totalPages}
-            className={`px-3 py-1 rounded ml-3 ${currentPage === totalPages ? "bg-gray-300 cursor-not-allowed" : "bg-slate-200 hover:bg-slate-300"
-              }`}
-          >
-            Next
-            <ArrowRightIcon className="inline h-4 w-4 ml-2 mb-1" />
-          </button>
-          <div className="ml-4">
-            <span className="text-sm text-gray-700">
-              Showing {(currentPage - 1) * itemsPerPage + 1} to{" "}
-              {Math.min(currentPage * itemsPerPage, totalItems)} of{" "}
-              {totalItems} entries
-            </span>
-          </div>
-        </div>
-
-      </div>
-
-      {modalVisible && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg">
-            <h2 className="text-xl font-semibold mb-4">Confirm Status Change</h2>
-            <p>Are you sure you want to {categoryToToggle?.active === true ? 'inactivate' : 'activate'} the category <strong>{categoryToToggle.name}</strong>?</p>
-            <div className="mt-6 flex justify-end">
+            {/* Page Number Buttons */}
+            {getPageNumbers().map((page) => (
               <button
-                onClick={() => setModalVisible(false)}
-                className="bg-gray-300 text-gray-800 rounded-lg px-4 py-2 mr-2"
+                key={page}
+                onClick={() => setCurrentPage(page)}
+                className={`px-3 py-1 rounded mx-1 ${currentPage === page ? "bg-blue-500 text-white" : "bg-slate-200 hover:bg-blue-100"
+                  }`}
               >
-                Cancel
+                {page}
               </button>
-             <button
-                                onClick={confirmToggleActiveStatus}
-                                disabled={isConfirmDisabled}
-                                className={`bg-blue-500 text-white rounded-md px-4 py-2 ${isConfirmDisabled ? 'opacity-50 cursor-not-allowed' : ''
-                                    }`}
-                            >
-                                {isConfirmDisabled ? 'Processing...' : 'Confirm'}
-                            </button>
+            ))}
+
+            {/* Page Count Info */}
+            <span className="text-sm text-gray-700 mx-2">of {totalPages} pages</span>
+
+            {/* Next Button */}
+            <button
+              onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+              disabled={currentPage === totalPages}
+              className={`px-3 py-1 rounded ml-3 ${currentPage === totalPages ? "bg-gray-300 cursor-not-allowed" : "bg-slate-200 hover:bg-slate-300"
+                }`}
+            >
+              Next
+              <ArrowRightIcon className="inline h-4 w-4 ml-2 mb-1" />
+            </button>
+            <div className="ml-4">
+              <span className="text-sm text-gray-700">
+                Showing {(currentPage - 1) * itemsPerPage + 1} to{" "}
+                {Math.min(currentPage * itemsPerPage, totalItems)} of{" "}
+                {totalItems} entries
+              </span>
             </div>
           </div>
+
         </div>
-      )}
-    </div>
-  );
+
+        {modalVisible && (
+          <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <h2 className="text-xl font-semibold mb-4">Confirm Status Change</h2>
+              <p>Are you sure you want to {categoryToToggle?.active === true ? 'inactivate' : 'activate'} the category <strong>{categoryToToggle.name}</strong>?</p>
+              <div className="mt-6 flex justify-end">
+                <button
+                  onClick={() => setModalVisible(false)}
+                  className="bg-gray-300 text-gray-800 rounded-lg px-4 py-2 mr-2"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={confirmToggleActiveStatus}
+                  disabled={isConfirmDisabled}
+                  className={`bg-blue-500 text-white rounded-md px-4 py-2 ${isConfirmDisabled ? 'opacity-50 cursor-not-allowed' : ''
+                    }`}
+                >
+                  {isConfirmDisabled ? 'Processing...' : 'Confirm'}
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      );
 };
 
-export default Category;
+      export default Category;
