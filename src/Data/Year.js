fc3be24811e568985da28d1b4d61095afb8bf11d
“HariomBhardwaj19” <hbstudent1997@gmail.com>
2025-06-09 15:52:37 +0530

    year css

--- src/Data/Year.js
+++ src/Data/Year.js
@@ -207,9 +207,10 @@ const Year = () => {
           />
         )}
         <div className="mb-4 bg-slate-100 p-2 rounded-lg">
-          <div className="flex gap-6">
-            <div className="w-4/5 grid grid-cols-1 sm:grid-cols-2 gap-6">
-              <label htmlFor="name" className="block text-md font-medium text-gray-700">
+          <div className="flex gap-6 ">
+            <div className="flex w-1/2 gap-6">
+              <label htmlFor="name" className="w-full block text-md font-medium text-gray-700 flex-1">
+                Select Year
                 <input
                   type="number"
                   min="1900"
@@ -224,20 +225,19 @@ const Year = () => {
                 />
               </label>
 
-
-            </div>
-            <div className="w-1/5 flex items-end">
-              {editingIndex === null ? (
-                <button onClick={handleAddYear}
-                  className="bg-blue-900 text-white rounded-2xl p-2 w-full text-sm flex items-center justify-center">
-                  <PlusCircleIcon className="h-5 w-5 mr-1" /> Add Year
-                </button>
-              ) : (
-                <button onClick={handleSaveEdit}
-                  className="bg-blue-900 text-white rounded-2xl p-2 w-full text-sm flex items-center justify-center">
-                  <CheckCircleIcon className="h-5 w-5 mr-1" /> Update
-                </button>
-              )}
+              <div className=" flex items-end">
+                {editingIndex === null ? (
+                  <button onClick={handleAddYear}
+                    className="bg-blue-900 text-white rounded-2xl p-2  text-sm flex items-center justify-center">
+                    <PlusCircleIcon className="h-5 w-5 mr-1" /> Add Year
+                  </button>
+                ) : (
+                  <button onClick={handleSaveEdit}
+                    className="bg-blue-900 text-white rounded-2xl p-2 text-sm flex items-center justify-center">
+                    <CheckCircleIcon className="h-5 w-5 mr-1" /> Update
+                  </button>
+                )}
+              </div>
             </div>
           </div>
         </div>
@@ -371,37 +371,37 @@ const Year = () => {
             </span>
           </div>
         </div>
-    </div>
-
-        {
-    modalVisible && (
-      <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h2 className="text-xl font-semibold mb-4">Confirm Status Change</h2>
-          <p>Are you sure you want to {yearToToggle?.isActive === 1 ? 'deactivate' : 'activate'} the year <strong>{yearToToggle.year}</strong>?</p>
-          <div className="mt-6 flex justify-end">
-            <button
-              onClick={() => setModalVisible(false)}
-              className="bg-gray-300 text-gray-800 rounded-lg px-4 py-2 mr-2"
-            >
-              Cancel
-            </button>
-            <button
-              onClick={confirmToggleActiveStatus}
-              disabled={isConfirmDisabled}
-              className={`bg-blue-500 text-white rounded-md px-4 py-2 ${isConfirmDisabled ? 'opacity-50 cursor-not-allowed' : ''
-                }`}
-            >
-              {isConfirmDisabled ? 'Processing...' : 'Confirm'}
-            </button>
-          </div>
-        </div>
       </div>
-    )
-  }
 
-      </div >
-      );
+      {
+        modalVisible && (
+          <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <h2 className="text-xl font-semibold mb-4">Confirm Status Change</h2>
+              <p>Are you sure you want to {yearToToggle?.isActive === 1 ? 'deactivate' : 'activate'} the year <strong>{yearToToggle.year}</strong>?</p>
+              <div className="mt-6 flex justify-end">
+                <button
+                  onClick={() => setModalVisible(false)}
+                  className="bg-gray-300 text-gray-800 rounded-lg px-4 py-2 mr-2"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={confirmToggleActiveStatus}
+                  disabled={isConfirmDisabled}
+                  className={`bg-blue-500 text-white rounded-md px-4 py-2 ${isConfirmDisabled ? 'opacity-50 cursor-not-allowed' : ''
+                    }`}
+                >
+                  {isConfirmDisabled ? 'Processing...' : 'Confirm'}
+                </button>
+              </div>
+            </div>
+          </div>
+        )
+      }
+
+    </div >
+  );
 };
 
 export default Year;
