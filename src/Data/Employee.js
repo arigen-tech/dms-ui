05c8c05494abfca18c89ec84be658de11cb69e73
“HariomBhardwaj19” <hbstudent1997@gmail.com>
2025-06-11 12:21:18 +0530

    employee

--- src/Data/Employee.js
+++ src/Data/Employee.js
@@ -607,7 +607,7 @@ const UserAddEmployee = () => {
 
   return (
     <div className="px-2">
-      <h1 className="ext-2xl mb-1 font-semibold">USERS</h1>
+      <h1 className="text-2xl mb-1 font-semibold">USERS</h1>
       <div className="bg-white p-1 rounded-lg shadow-sm">
         {error && <p className="text-red-500">{error}</p>}
         {/* Success Message */}
