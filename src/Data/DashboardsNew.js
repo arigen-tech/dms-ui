3c6c7d98d37ee95fcc01322be3ded935b2ed36df
Mrdheerajcs <mrdheerajtech@gmail.com>
2026-03-11 10:38:19 +0530

    ..

--- src/Data/DashboardsNew.js
+++ src/Data/DashboardsNew.js
@@ -1,11 +1,11 @@
-import {MAIN_DASHBOARD_URL} from "../API/apiConfig";
+// import {MAIN_DASHBOARD_URL} from "../API/apiConfig";
 
-export default function DashboardsNew() {
-  return (
-    <iframe
-      src={MAIN_DASHBOARD_URL}
-      style={{ width: "100%", height: "100vh", border: "none" }}
-      title="Dashboard"
-    />
-  );
-}
+// export default function DashboardsNew() {
+//   return (
+//     <iframe
+//       src={MAIN_DASHBOARD_URL}
+//       style={{ width: "100%", height: "100vh", border: "none" }}
+//       title="Dashboard"
+//     />
+//   );
+// }
